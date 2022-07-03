5704fc5bc9f09803db5d62cc78f657001767a54f
morinaga113 <107459116+morinaga113@users.noreply.github.com>
2022-07-04 00:09:31 +0800

    大圖輪播index2無法自適應

---
+++ public/js/index-script.js
@@ -0,0 +1,8 @@
+    // 小於768執行
+    $('#btn-nav-switch').on('click', function(){
+        $('nav').toggleClass('active')
+    })// btn-nav-switch end
+    
+    $('nav>ul>li').on('click',function(){
+        $(this).find('.submenu').toggleClass('active')
+    })// nav>ul>li end
